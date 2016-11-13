c781f5b1f83a5d7ca68d9a075b025f73875a6115
826892357@qq.com <826892357@qq.com>
2016-11-13 21:48:31 +0800

    This is my GitTestMerge -001

---
+++ src/main/java/com/jlc/api/GitTest/gitTestMerge.java
@@ -0,0 +1,10 @@
+package com.jlc.api.GitTest;
+
+/**
+ * Created by Administrator on 2016/11/13 0013.
+ */
+public class gitTestMerge {
+    public static void main(String[] args) {
+        System.out.println("This is my GitTestMerge");
+    }
+}
