2d1faaec4b77529d786b2a9e4d9d85bf5d00c99c
826892357@qq.com <826892357@qq.com>
2016-11-13 22:52:09 +0800

    commit -------

--- src/main/java/com/jlc/api/GitTest/gitTestMerge.java
+++ src/main/java/com/jlc/api/GitTest/gitTestMerge.java
@@ -5,7 +5,7 @@ package com.jlc.api.GitTest;
  */
 public class gitTestMerge {
     public static void main(String[] args) {
-        System.out.println("This is my GitTestMerge=======");
+        System.out.println("This is my GitTestMerge--------");
         System.out.println("This is my GitTestMerge03");
     }
 }
