7d06c0de7cd6aeb5afea3689834806bca6f905a4
826892357@qq.com <826892357@qq.com>
2016-11-14 21:59:08 +0800

    master/merge so easy

--- src/main/java/com/jlc/api/GitTest/gitTestMerge.java
+++ src/main/java/com/jlc/api/GitTest/gitTestMerge.java
@@ -7,5 +7,6 @@ public class gitTestMerge {
     public static void main(String[] args) {
         System.out.println("This is my GitTestMerge5555555555");
         System.out.println("This is my GitTestMerge025645646");
+        System.out.println("merge so easy! easy!");
     }
 }
