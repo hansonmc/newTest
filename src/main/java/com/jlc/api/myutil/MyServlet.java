3eed430b4d00695bc2f95ee5d00bad28c76e07cd
mhan <826892357@qq.com>
2017-03-20 22:07:42 +0800

    plays1

---
+++ src/main/java/com/jlc/api/myutil/MyServlet.java
@@ -0,0 +1,22 @@
+package com.jlc.api.myutil;
+
+import com.sun.org.apache.xpath.internal.SourceTree;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.*;
+import java.io.IOException;
+
+/**
+ * Created by menghan on 2017/3/19.
+ */
+public class MyServlet extends HttpServlet {
+    @Override
+    public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        HttpSession session = null;
+        System.out.println(session = req.getSession(false));
+        System.out.println(session.getId());
+        System.out.println("inin");
+    }
+
+
+}
